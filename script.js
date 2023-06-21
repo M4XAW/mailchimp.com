5d9d2bfdc6163d0c0b3c2562a0464b86aa6522a6
M4XAW <m.picault@ecole-ipssi.net>
2023-06-21 16:44:07 +0200

    maj

--- script.js
+++ script.js
@@ -31,3 +31,53 @@ $("#close").click(function () {
     300
   );
 });
+
+// window.addEventListener('scroll', function() {
+//   var maDiv = document.querySelector('.description');
+//   var position = maDiv.getBoundingClientRect();
+
+//   if (position.top < window.innerHeight && position.bottom >= 0) {
+//     maDiv.style.opacity = 1;
+//     maDiv.style.transform = 'translateY(0px)';
+//   } else {
+//     maDiv.style.opacity = 0;
+//     maDiv.style.transform = 'translateY(-300px)';
+//   }
+// });
+
+const animatedElements = document.querySelectorAll(".animated");
+const animatedElementsRight = document.querySelectorAll(".animated-right");
+
+window.addEventListener("scroll", animate);
+
+function animate() {
+  animatedElements.forEach((elem) => {
+    const distView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+
+    if (distView < 0) {
+      elem.classList.add("active");
+    } else {
+      elem.classList.remove("active");
+    }
+  });
+
+  animatedElementsRight.forEach((elem) => {
+    const distView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+
+    if (distView < 0) {
+      elem.classList.add("active");
+    } else {
+      elem.classList.remove("active");
+    }
+  });
+}
+
+const scrollProgress = document.getElementById("scroll-progress");
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
